ec84b708d690ed1f4ce6aebbbd883ad7879bc867
499244188 <499244188@qq.com>
2016-03-08 17:41:01 +0800

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,33 @@
+
+x = 1
+#y = (x = x + 1)  报错
+y = x = x + 1
+print(y)
+
+m = 5
+print(m)
+
+m %= 3
+print(m)
+
+
+m = 7
+
+m **= 2
+print(m)
+
+aList = [123,'xyz']
+print(aList)
+aList += [45.6e7]
+print(aList)
+
+x = y = z = w = 12
+print(x,y,z,w)
+
+x,y,z, = 3,5,7
+print(x,y,z)
+
+x,y  = y,x
+print(x,y)
+
+
